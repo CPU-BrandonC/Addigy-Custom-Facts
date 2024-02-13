f857a907b01322af14de772d24ceaf5cb2c01576
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-02-13 15:26:05 -0600

    Added notes

--- run_as_logged_in_user.zsh
+++ run_as_logged_in_user.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+# This isn't an Addigy Custom Fact but it's useful
 # See https://scriptingosx.com/2020/08/running-a-command-as-another-user/
 
 currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
