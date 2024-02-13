24c33a9297c439648e740a822bc3cbccd049a891
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-02-13 15:22:44 -0600

    added new script

---
+++ is_application_installed.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+application_name="NeoFinder.app"
+currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+
+if [ -d "/Applications/$application_name" ] || [ -d /Users/$currentUser/Applications/$application_name ]; then
+    echo "true"
+else
+    echo "false"
+fi
