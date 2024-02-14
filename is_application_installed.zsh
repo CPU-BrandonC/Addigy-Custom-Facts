f9b405a924b5e3a67bf3bdcb20180c4b84c2fdd6
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-02-14 08:10:39 -0600

    So apparently double-brackets are better

--- is_application_installed.zsh
+++ is_application_installed.zsh
@@ -3,7 +3,7 @@
 application_name="NeoFinder.app"
 currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
 
-if [ -d "/Applications/$application_name" ] || [ -d /Users/$currentUser/Applications/$application_name ]; then
+if [[ -d "/Applications/$application_name" ]] || [[ -d /Users/$currentUser/Applications/$application_name ]]; then
     echo "true"
 else
     echo "false"
