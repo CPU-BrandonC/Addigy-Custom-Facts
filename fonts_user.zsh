3e754c49a6d210fe6a15076d6f809c6d912c4b91
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-02-22 10:18:16 -0600

    Improved logic for Addigy filtering

--- fonts_user.zsh
+++ fonts_user.zsh
@@ -2,4 +2,9 @@
 
 currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
 
-sudo ls "/Users/$currentUser/Library/Fonts"
+installed_fonts=$(sudo ls -I "/Users/$currentUser/Library/Fonts")
+if [[ -z $installed_fonts ]]; then
+    echo "N/A"
+else
+    echo "$installed_fonts"
+fi
